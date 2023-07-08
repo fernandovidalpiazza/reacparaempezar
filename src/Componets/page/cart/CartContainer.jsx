5904efc853dcad05d5f5547c001856de62779a5c
fernandovidalpiazza <fervidalpiazza@gmail.com>
2023-07-08 19:17:55 -0300

    entrega2

--- src/Componets/page/cart/CartContainer.jsx
+++ src/Componets/page/cart/CartContainer.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const CartContainer = () => {
   return (
-    <div><h1>estoy en el carro</h1></div>
-  )
-}
+    <div>
+      <h1>estoy en el carro</h1>
+    </div>
+  );
+};
 
-export default CartContainer
+export default CartContainer;
